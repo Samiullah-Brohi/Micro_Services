0e14cac578efea076ae1a4925f1616420a3127b8
Samiullah <samiullah.abdulqadir@cloudsolutions.com.sa>
2026-02-12 10:23:58 +0300

    Add new endpoint in product service

--- Micro_Services/Controllers/ProductsController.cs
+++ Micro_Services/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Micro_Services.Controllers
 {
@@ -12,6 +13,15 @@ namespace Micro_Services.Controllers
         public IEnumerable<string> Get()
         {
             return new[] { "Laptop", "Phone", "Tablet" };
+        }
+
+        public IEnumerable<string> GetByName(string ProductName)
+        {
+
+            var products = new[] { "Laptop", "Phone", "Tablet" };
+            var filteredProducts = products.Where(p => p.Contains(ProductName, StringComparison.OrdinalIgnoreCase));
+
+            return filteredProducts;
         }
     }
 }
